4b1eb8d8ad44be7ea3be50adcd3607b9b7edcf5d
marco <129217375+watonmarco@users.noreply.github.com>
2023-06-09 00:55:34 -0400

    poroto v2
    p

--- porototipo/porototipo.py
+++ porototipo/porototipo.py
@@ -1,15 +1,15 @@
 # importar flask y otra mugre para llamar otros archivos
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # hacer una página, template_folder es para que encuentre donde estan los html y cosas
-app = Flask(__name__,template_folder='plantillas')
+app = Flask(__name__,template_folder='plantillas',static_folder='estatua') 
 
 # le asignamo una ruta a la página
 @app.route("/")   # "/" para que quede como la default, en este caso http://127.0.0.1:5000/  <-(tiene el "/")
 # esta ruta va a ser la página principal
 def home():
     # usando el render_template se se importó antes para llamar el .html
-    return render_template("casa.html")
+    return render_template("main.html")
 
 # que es rutu
 @app.route("/pag")
@@ -20,6 +20,10 @@ def pag():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/map")
+def map():
+    return render_template("casa.html")
 
 # acá para que la página abra
 if __name__ == "__main__":

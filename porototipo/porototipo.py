41f32ebac62fa2de4047986fbca756852794b848
Chiit0 <129217478+Chiit0@users.noreply.github.com>
2023-07-04 14:58:23 -0400

    conexión a base de datos

--- porototipo/porototipo.py
+++ porototipo/porototipo.py
@@ -1,5 +1,11 @@
 # importar flask y otra mugre para llamar otros archivos
 from flask import Flask, render_template, request
+import sqlite3
+import os
+import json
+
+currentdirectory = os.path.dirname(os.path.abspath(__file__))
+connection = sqlite3.connect(currentdirectory + "\proyecto_full.db", check_same_thread=False)
 
 # hacer una página, template_folder es para que encuentre donde estan los html y cosas
 app = Flask(__name__,template_folder='plantillas',static_folder='estatua') 
@@ -25,14 +31,26 @@ def about():
 def TyC():
     return render_template("terminos-y-servicios.html")
 
-'''   ya le puse el mapa a la página principal asi que esto no es necesario   '''
+#'''   ya le puse el mapa a la página principal asi que esto no es necesario   '''
 @app.route("/map")
 def map():
     return render_template("casa.html")
 
-@app.route("/pelao")
-def pelao():
-    return render_template("mapa-pelao.html")
+#MAPA CON RUTA
+@app.route("/ruta1a")
+def ruta1a():
+    query1 = "SELECT latitud, longitud from paradero"
+    cursor = connection.cursor()
+    resultado = cursor.execute(query1)
+    resultado = resultado.fetchall()
+
+    query2 = "SELECT latitud, longitud from Ruta WHERE num = '1a'"
+    resultado2 = cursor.execute(query2)
+    resultado2 = resultado2.fetchall()
+    for i in range(len(resultado2)):
+        resultado2[i] = list(resultado2[i])
+    return render_template("mapa_ruta1a.html", stops = resultado, ruta1a = resultado2)
+
 
 # acá para que la página abra
 if __name__ == "__main__":
